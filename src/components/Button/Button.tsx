9e5a580cbed9719169a3c8c417cfd491f81d9276
Rustam Abdrakhmanov <ruabdrakhmanov@gmail.com>
2024-07-12 12:36:47 +0600

    feat: convert Button to functional component, add default props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,12 @@
 import { ButtonProps } from './Button.props';
 import styles from './Button.module.css';
 
-export function Button({ children, appearance, size, ...props }: ButtonProps) {
+export function Button({
+  children,
+  appearance = 'primary',
+  size = 'm',
+  ...props
+}: ButtonProps) {
   return (
     <button
       className={`${styles['button']} ${styles[appearance]} ${styles[size]}`}
