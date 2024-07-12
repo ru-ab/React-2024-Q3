2b28a85ee0649c4052c452dca5f87f89834cd9ee
Rustam Abdrakhmanov <ruabdrakhmanov@gmail.com>
2024-07-12 16:31:29 +0600

    feat: convert App to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { Component } from 'react';
 import { MainPage } from './pages';
 import { ErrorBoundary, Fallback } from './components';
 
-export class App extends Component {
-  render(): JSX.Element {
-    return (
-      <ErrorBoundary fallback={<Fallback />}>
-        <MainPage />
-      </ErrorBoundary>
-    );
-  }
+export function App() {
+  return (
+    <ErrorBoundary fallback={<Fallback />}>
+      <MainPage />
+    </ErrorBoundary>
+  );
 }
