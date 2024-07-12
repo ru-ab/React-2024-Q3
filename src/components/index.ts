0c2b85ffa81590aade8ea607f302203dd068c1dc
Rustam Abdrakhmanov <ruabdrakhmanov@gmail.com>
2024-07-12 15:40:02 +0600

    feat: convert Spinner to functional component, add tests

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { ListItems } from './ListItems/ListItems';
 export { BuggyButton } from './BuggyButton/BuggyButton';
 export { ErrorBoundary } from './ErrorBoundary/ErrorBoundary';
 export { Fallback } from './Fallback/Fallback';
+export { Spinner } from './Spinner/Spinner';
