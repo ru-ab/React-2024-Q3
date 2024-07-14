d9b5aafa4182cb62da045aa63562a2e82b0fddec
Rustam Abdrakhmanov <ruabdrakhmanov@gmail.com>
2024-07-15 01:11:05 +0600

    feat: add test on close panel button

--- src/tests/components/DetailedCard/DetailedCard.test.tsx
+++ src/tests/components/DetailedCard/DetailedCard.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { DetailedCard } from '../../../components';
 import { CardType } from '../../../types';
+import userEvent from '@testing-library/user-event';
 
 vi.mock('react-router-dom');
 
@@ -72,5 +73,27 @@ describe('DetailedCard', () => {
     const spinner = screen.getByRole('progressbar');
 
     expect(spinner).toBeInTheDocument();
+  });
+
+  it('should remove search param upon clicking close panel button', async () => {
+    const card = {
+      id: '1',
+      name: 'card1',
+      flavorText: 'flavorText1',
+      images: { small: 'small1', large: 'large1' },
+      attacks: ['attack1'],
+    } as unknown as CardType;
+
+    const { setSearchParamsMock } = await renderComponent({
+      item: card,
+      loading: false,
+    });
+
+    const closeButton = screen.getByRole('button');
+
+    const user = userEvent.setup();
+    await user.click(closeButton);
+
+    expect(setSearchParamsMock).toHaveBeenCalled();
   });
 });
