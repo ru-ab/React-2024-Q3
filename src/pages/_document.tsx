6691f69c774ce853653527d2e3f0742830eb6a78
Rustam Abdrakhmanov <ruabdrakhmanov@gmail.com>
2024-08-05 12:27:09 +0600

    feat: add favicon and page title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/pokeball.png" />
+        <title>Pokémon TCG</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
